cb7fe29d39fee0b9329d8ea7565c13323d620d07
saichandesh <saichandesh@gmail.com>
2016-10-11 20:11:42 -0400

    added socket.io

--- app-server.js
+++ app-server.js
@@ -1,10 +1,27 @@
 var express = require('express');
+var socket = require('socket.io');
 
 var app = express();
+
+var connections = [];
 
 app.use(express.static("./node_modules/bootstrap/dist"));
 app.use(express.static("./"));
 
-app.listen(8080,function(){
+var server = app.listen(8080,function(){
 	console.log("Server running at port:8080");
+});
+
+var io = socket.listen(server);
+
+io.sockets.on('connection', function(socket){
+
+	socket.once('disconnect',function(){
+		connections.splice(connections.indexOf(socket), 1);
+		socket.disconnect();
+		console.log("Number of sockets Remaining: " + connections.length);
+	});
+
+	connections.push(socket);
+	console.log("Number of sockets connected: " + connections.length);
 });
--- components/APP.js
+++ components/APP.js
@@ -1,9 +1,35 @@
 var React = require('react');
+var io = require('socket.io-client');
+var Header = require('./partials/Header');
 
 var APP = React.createClass({
 
+	getInitialState(){
+		return {
+			status : 'disconnected'
+		}
+	},
+
+	componentWillMount(){
+		this.socket = io('http://localhost:8080');
+		this.socket.on('connect', this.connect);
+		this.socket.on('disconnect', this.disconnect);
+	},
+
+	connect(){
+		this.setState({ status : 'connected'});
+	},
+
+	disconnect(){
+		this.setState({ status : 'disconnected'});
+	},
+
 	render(){
-		return (<h1>Hello world from react</h1>);
+		return (
+					<div>
+						<Header title="New Header" status={this.state.status}/>
+					</div>
+				);
 	}
 });
 
